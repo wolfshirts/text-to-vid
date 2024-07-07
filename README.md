69a54a45ee47eb352bd1f85e034703c4bc39cd09
wolfshirts <wolfshirts@protonmail.com>
2024-07-07 10:43:38 -0700

    building out the config

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # text-to-vid
 text to video generator
+
+uses https://huggingface.co/ali-vilab/text-to-video-ms-1.7b
+
+## Usage
+* Fork the project
+* Configure on CCI if necessary
+* Edit the prompt
+* Push
+* Get the artifact from the latest build, this is your new video
--- vidmaker.py
+++ vidmaker.py
@@ -6,6 +6,7 @@ pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", tor
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe.enable_model_cpu_offload()
 
-prompt = "Spiderman is surfing"
+with open("prompt.txt", "r") as f:
+    prompt = f.read()
 video_frames = pipe(prompt, num_inference_steps=25).frames
 video_path = export_to_video(video_frames)
